6ac2c2afc13d918cf0fb0cfc505bf3a6ab3b5430
tamayura8823spirit <mh61yorotouch@yahoo.co.jp>
2021-11-12 19:34:53 +0900

    Create main.js

---
+++ 参考用コード集/JavaScript[完全]入門/Canvas - パスの描画/main.js
@@ -0,0 +1,25 @@
+const canvas = document.querySelector('#canvas');
+const context = canvas.getContext('2d');
+
+// 設定
+context.fillStyle = '#faa';
+context.strokeStyle = '44f';
+context.lineWidth = 8;
+context.lineCap = 'round';
+context.lineJoin = 'roung';
+
+context.beginPath();
+
+// 左上に三角
+context.moveTo(100, 50);
+context.lineTo(10, 250);
+context.lineTo(190, 250);
+
+// 右上に三角
+context.moveTo(300, 50);
+context.lineTo(210, 250);
+context.lineTo(390, 250);
+context.closePath()
+
+context.fill();
+context.stroke();
