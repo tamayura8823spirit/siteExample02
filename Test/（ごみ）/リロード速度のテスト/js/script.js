315c891a3a9d8c6e060a2e3bf3ff78cb79d7700a
tamayura8823spirit <mh61yorotouch@yahoo.co.jp>
2023-03-23 16:25:35 +0900

    a

---
+++ Test/（ごみ）/リロード速度のテスト/js/script.js
@@ -0,0 +1,27 @@
+// $(window).on('load', function () {
+  $(document).ready(function() {
+
+  $('#splash-logo').addClass('fade-in');
+  
+
+
+
+  //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+  setTimeout(function() {
+    $('#splash-logo').addClass('slide-up');
+  }, 2600); // 2秒遅延
+  
+  // , function () {//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+
+  //   $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+
+  // });
+  //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+
+
+  //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
+  $('.splashbg').on('animationend', function () {
+    //この中に動かしたいJSを記載
+  });
+  //=====ここまで背景が伸びた後に動かしたいJSをまとめる
+});
